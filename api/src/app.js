642518b9b6e9509e90f7b23891378aafa8ef76ed
Bryan Bowman <bryan@techroller.com>
2019-05-28 19:27:33 -0600

    Removing unnecessary UI stuff

--- api/src/app.js
+++ api/src/app.js
@@ -15,15 +15,10 @@ const jobsRouter = require('./routes/jobs');
 
 const app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, '../public')));
 
 app.use('/', indexRouter);
 app.use('/jobs', jobsRouter);
